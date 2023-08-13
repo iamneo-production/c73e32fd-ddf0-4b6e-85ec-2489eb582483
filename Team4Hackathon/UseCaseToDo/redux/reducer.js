ee43521986d5df1ed08ea56c7869e3bf68aa264a
Divya-Varatharajan <divyav.cs.006@gmail.com>
2023-08-13 18:00:28 +0000

    Reducer file changes added

--- Team4Hackathon/UseCaseToDo/redux/reducer.js
+++ Team4Hackathon/UseCaseToDo/redux/reducer.js
@@ -1,7 +1,34 @@
+function getId(state) {
+  return state.todos.reduce((maxId, todo) => {
+    return Math.max(todo.id, maxId)
+  }, -1) + 1
+}
 let reducer = (state, action) => {
-    switch (action.type) {
-      default:
-        return state;
+  switch (action.type) {
+    case 'ADD_TODO':
+      return Object.assign({}, state, {
+        todos: [{
+          title: action.title,
+          completed: false,
+          id: getId(state)
+        }, ...state.todos]
+      })
+    case 'COMPLETE_TODO':
+      return Object.assign({}, state, {
+        todos: state.todos.map((todo) => {
+          return todo.id === action.id ?
+          Object.assign({}, todo, {updated: false, completed: ! 
+          todo.completed}) : todo
+        })
+      })
+    case 'DELETE_TODO':
+      return Object.assign({}, state, {
+        todos: state.todos.filter((todo) => {
+          return todo.id !== action.id
+        })
+      })
+    default:
+      return state;
     }
-  }
-  export default reducer
+}
+export default reducer
