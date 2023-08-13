56f98311f9e1234a9a7645ba05c69268972956fc
Divya-Varatharajan <divyav.cs.006@gmail.com>
2023-08-13 15:13:32 +0000

    Basic ToDo app added

---
+++ Team4Hackathon/UseCaseToDo/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import TodoInput from './TodoInput'
+import TodoList from './TodoList'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import actions from '../redux/actions'
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="header">Todos</h1>
+        <TodoInput addTodo={this.props.actions.addTodo}/>
+        <TodoList actions={this.props.actions} todos=       {this.props.todos}/>
+      </div>
+    )
+  }
+}
+function mapStateToProps(state) {
+  return state
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App)
