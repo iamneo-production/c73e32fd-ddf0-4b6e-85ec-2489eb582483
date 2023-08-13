731c619c9adc96b71afc136d1841d41c0457fcff
Divya-Varatharajan <divyav.cs.006@gmail.com>
2023-08-13 18:19:04 +0000

    updated

--- Team4Hackathon/UseCaseToDo/redux/actions.js
+++ Team4Hackathon/UseCaseToDo/redux/actions.js
@@ -1,3 +1,21 @@
 let actions = {
-}
-export default actions
+    addTodo: function(title) {
+      return {
+        type: 'ADD_TODO',
+        title: title,
+      }
+    },
+    completeTodo: function(id) {
+      return {
+        type: 'COMPLETE_TODO',
+        id: id
+      }
+    },
+    deleteTodo: function(id) {
+      return {
+        type: 'DELETE_TODO',
+        id: id
+       }
+    }
+  }
+  export default actions
